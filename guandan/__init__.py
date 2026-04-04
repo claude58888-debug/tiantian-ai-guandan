ac7beecc6d54282427b1da93429223c728bbe06f
claude58888-debug <claude58888@gmail.com>
2026-04-04 23:59:32 +0900

    Create __init__.py

---
+++ guandan/__init__.py
@@ -0,0 +1,6 @@
+"""Guandan card game AI agent package."""
+
+from guandan.models import Card, Deck, Hand, Rank, Suit, JokerType
+
+__all__ = ['Card', 'Deck', 'Hand', 'Rank', 'Suit', 'JokerType']
+__version__ = '0.1.0'
